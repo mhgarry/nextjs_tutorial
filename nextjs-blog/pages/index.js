97e9323db57b74e9912696658626f6bada948a98
Matt <mhgarry92@gmail.com>
2023-06-14 18:19:07 -0400

    ready for deploy

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -12,11 +12,7 @@ export default function Home({ allPostsData }) {
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
-				<p>[Your Self Introduction]</p>
-				<p>
-					(This is a sample website - you’ll be building a site like this in{" "}
-					<a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
-				</p>
+				<p>Hi, I'm Matt, I'm a full stack web developer. This is my first Next.js project and I learned to build this application by following this Next.js <a href="https://nextjs.org/learn">tutorial</a></p>
 			</section>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
 				<h2 className={utilStyles.headingLg}>Blog</h2>
